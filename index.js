de58305ab7a2f29c0f0fcca9e8f74f260ecc4b55
dg1223 <alavi1223@hotmail.com>
2023-05-17 01:43:52 -0400

    show data on orders page using POST

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+/* app.use(express()); // Does not handle JSON data when received by POST */
+app.use(express.json()); // Hnadles JSON POST requests
 
 /* Connect to MongoDB */
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dg1223.za2ri3i.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,13 +25,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const productCollection = client.db("emaJohnDB").collection("products");
 
     // GET
     app.get("/products", async (req, res) => {
-      console.log(req.query);
+      console.log("GET /products: ", req.query);
 
       // pagination
       const page = parseInt(req.query.page) || 0;
@@ -45,12 +46,24 @@ async function run() {
       res.send(result);
     });
 
-    // Pagination
+    /* Pagination starts */
     app.get("/totalProducts", async (req, res) => {
       const result = await productCollection.estimatedDocumentCount();
       // Must wrap numbers as objects when sending a response
       res.send({ totalProducts: result });
     });
+
+    // Receive products ids to client-side cart
+    app.post("/productsById", async (req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map((id) => new ObjectId(id));
+      const query = { _id: { $in: objectIds } };
+      console.log("POST /productsById", req.body);
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    /* Pagination ends */
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
